d5883bad27922b684afe12e04f505c3128961797
L1m3Kun <sungku2757@gmail.com>
2026-05-01 18:32:30 +0900

    fix: api env updated ASApiUrl to func #12

--- lib/env/api.env.ts
+++ lib/env/api.env.ts
@@ -1,7 +1,23 @@
 import { requireEnv } from "./_config.env";
 
 const api = {
-  AsApiUrl: requireEnv("AS_API_URL"),
+  // @TODO : t3-oss/env-nextjs 도입 - 도입이유: 테스트 격리, 빌드 타임 검증, 타입 안정 보장
+  /**
+    import { createEnv } from "@t3-oss/env-nextjs";                                                                                                                                
+    import { z } from "zod";
+                                                                                                                                                                                  
+    export const env = createEnv({                                                                                                                                               
+      server: {
+        AS_API_URL: z.string().url(),
+      },                                                                                                                                                                           
+      runtimeEnv: {
+        AS_API_URL: process.env.AS_API_URL,                                                                                                                                        
+      },                                                                                                                                                                         
+    });
+   */
+  get AsApiUrl() {
+    return requireEnv("AS_API_URL");
+  },
 };
 
 export { api };
